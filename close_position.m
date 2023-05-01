d15d365db93257a5938725a4946e77927fd73975
ignacio-fernandez <ifndez3@gmail.com>
2023-05-01 17:13:36 -0400

    delete unnecessary files. separate useful functions into own files

---
+++ close_position.m
@@ -0,0 +1,13 @@
+function close_position()
+    portfolioData = IBMatlab('action','portfolio', 'type','positions');
+    tickersCO = {portfolioData.localSymbol};
+    for tCOs = tickersCO
+        tCO = string(tCOs);
+        pos = Position(tCO);
+        if pos < 0
+            IBMatlab('action','BUY','symbol',tCO,'quantity',-pos,'type','MKT');
+        elseif pos > 0
+            IBMatlab('action','SELL','symbol',tCO,'quantity',pos,'type','MKT');
+        end
+    end
+end
