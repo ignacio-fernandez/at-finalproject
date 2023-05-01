fd1b017c48ae2abec9861d4eb367e75b7a49388a
ignacio-fernandez <ifndez3@gmail.com>
2023-05-01 16:54:02 -0400

    delete vars

--- macd.m
+++ macd.m
@@ -1,8 +1,6 @@
-function [S, macd_val, avg_val] = macd(X, slow, macd_window, fast)
+function [S] = macd(X, slow, macd_window, fast)
     macd_line = ema(X, fast) - ema(X, slow);
     signal_line = ema(macd_line, macd_window);
-    macd_val = macd_line(end);
-    avg_val = signal_line(end);
 
     if (macd_line(end-1) < signal_line(end-1)) && (macd_line(end) > signal_line(end))
         S = 1; % BUY
