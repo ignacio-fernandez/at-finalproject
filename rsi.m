8ef0220d3ee7d29452be8cdbccc161e98e79320c
ignacio-fernandez <ifndez3@gmail.com>
2023-05-01 17:06:56 -0400

    cleanup rsi file

---
+++ rsi.m
@@ -0,0 +1,21 @@
+function [S] = rsi(X, h, L, U)
+    R = diff(X);
+
+    UUt = R.*(R > 0);
+    DDt = -R.*(-R > 0);
+
+    RS = (sum(UUt(end-h:end)) / h) / (sum(DDt(end-h:end)) / h);
+    RSI = 100*(1 - 1/(1+RS));
+
+    RS_lag = (sum(UUt(end-h-1:end-1)) / h) / (sum(DDt(end-h-1:end-1)) / h);
+    RSI_lag = 100*(1 - 1/(1+RS_lag));
+
+    if RSI_lag < U && RSI > U
+        S = -1;
+    elseif RSI_lag > L && RSI < L
+        S = 1;
+    else
+        S = 0;
+    end
+
+end
