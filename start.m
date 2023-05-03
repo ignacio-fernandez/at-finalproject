4d876c22b239eca5f14909fc00abcdb8248ec89f
Donald Donnelly <ddonnelly12@fordham.edu>
2023-05-03 19:05:44 -0400

    Update start.m
    added getting the bar data from array of tickers. Still need to add user input for tickers.

--- start.m
+++ start.m
@@ -3,8 +3,18 @@ function start(handles)
     windowSize = 20;
     arraySize = 2^16;
     X = zeros(arraySize, 1);
-
+    
+    %tickers = %get user input here
+    tickers = ['AAPL', 'TSLA', 'NVDA'];
+    for t = tickers
+        IBMatlab('action','realtime', 'symbol',t,'QuotesNumber',inf,'QuotesBufferSize',50,'useRTH',1);
+    end
+    
     while (1)
+        bars = zeros(length(tickers),1);
+        for j = 1:length(tickers)
+            bars(j) = IBMatlab('action','realtime', 'symbol',tickers(j),'QuotesNumber',-1);
+        end
         if get(handles.stop_btn, 'userdata') % stop condition
             cla(handles.pnl_graph);
             break;
@@ -16,7 +26,7 @@ function start(handles)
             % execute trade
         end
         
-        p = portfolio();
+        p = portfolio(bars);
         curr_pnl = total_pnl(p);
         set(handles.portfolio_table, 'Data', p);
         set(handles.total_pnl_text, 'String', curr_pnl);
