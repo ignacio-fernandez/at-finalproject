8046d1f101871ded4f4f9486ec0569e887d0697c
ray <ychi11@fordham.edu>
2023-05-03 16:46:25 -0400

    Update kdj.m
    kdj function changes

--- kdj.m
+++ kdj.m
@@ -1,34 +1,47 @@
-function [K, D, J] = kdj(high, low, close, window, k_window, d_window)
-% Computes the KDJ indicator for a given set of price data
-% Input:
-%   high: vector of high prices
-%   low: vector of low prices
-%   close: vector of closing prices
-%   window: look-back period for calculating the highest high and lowest low
-%   k_window: look-back period for calculating the %K line
-%   d_window: look-back period for calculating the %D line
-% Output:
-%   K: %K line
-%   D: %D line
-%   J: %J line
+function [signal] = kdj(bar_data, n, m1)
+% Inputs:
+%   - bar_data: 4-D array of bar data (open, high, low, close)
+%   - n: look-back period for calculating K line
+%   - m1: smoothing period for calculating D line
+%   - m2: smoothing period for calculating J line
+% Outputs:
+%   - K: array of K line values
+%   - D: array of D line values
+%   - J: array of J line values
+%   - signal: array of trading signals (0 = no signal, 1 = buy, -1 = sell)
 
-% Calculate highest high and lowest low over the past 'window' periods
-highest_high = highest(high, window);
-lowest_low = lowest(low, window);
+% Calculate RSV values
+highest_high = max(bar_data(:, 2), [], 2);
+lowest_low = min(bar_data(:, 3), [], 2);
+RSV = (bar_data(:, 4) - lowest_low) ./ (highest_high - lowest_low) * 100;
 
-% Calculate the %K line
-RSV = 100 * (close(-1) - lowest_low) ./ (highest_high - lowest_low);
+% Calculate K line
+K = sma(RSV, n, 1);
 
-K = 2/3*
+% Calculate D line
+D = sma(K, m1, 1);
 
-% Calculate the %D line
-D = sma(K, d_window);
-
-% Calculate the %J line
+% Calculate J line
 J = 3 * K - 2 * D;
 
-
-
-if 
-
+% Generate trading signals
+signal = zeros(size(J));
+signal(K < 20 & D < 20) = 1; % buy signal
+signal(K > 80 & D > 80) = -1; % sell signal
 end
+
+function out = sma(x, n, dim)
+% Simple moving average function
+persistent weights
+if isempty(weights) || length(weights) ~= n
+    weights = ones(n, 1) / n;
+end
+out = convn(x, weights, 'same');
+if dim == 1
+    out(1:n-1,:) = NaN;
+else
+    out(:,1:n-1) = NaN;
+end
+end
+
+
