c4c7d6c5ddb827ed649354e0008cdef19bdec970
BrandonChen0505 <121589746+BrandonChen0505@users.noreply.github.com>
2023-05-03 17:40:14 -0400

    Update execute_signals.m
    update

--- execute_signals.m
+++ execute_signals.m
@@ -1,21 +1,35 @@
-function Execution(dollar, symbol,signals)
+function orderId = execute_signals(dollar, symbol,signals, close)
 
+result = zeros(1, size(newArray, 2));
 
+for i = 1:size(signals, 2)
+    column = signals(:, i);
+    
+   
+    [uniqueNums, ~, indices] = unique(column);
+    counts = accumarray(indices, 1);
+    
+    maxCount = max(counts);
+    if maxCount > 1
+        maxIndices = find(counts == maxCount);
+        if length(maxIndices) == 1
+            result(i) = uniqueNums(maxIndices);
+        end
+    end
+end
 
-new_data = IBMatlab('action','query', 'symbol', symbol)
-last = new_data.lastPrice;
-bid1 = new_data.bidPrice;
-ask1 = new_data.askPrice;
-
-N = round(dollar / last, 0);
-
-random_nums = randi(1, N);
-normalized_nums = random_nums / sum(random_nums) * N;
-
-orderId = IBMatlab('action', BuySell, 'symbol', symbol, 'quantity',
-normalized_nums(i), 'type', 'MKT');
-
-rand_time = rand() * 2 + 1;
-pause(rand_time*0.5);
-
+action = result(-1);
+if action ~= 0
+    if action > 0
+        action = 'BUY';
+    else
+        action = 'SELL';
+    end
+    
+    N = round(dollar / close, 0);
+   
+    
+    orderId = IBMatlab('action', action, 'symbol', symbol, 'quantity', N, 'type', 'MKT');
+else
+    orderId = NaN;
 end
