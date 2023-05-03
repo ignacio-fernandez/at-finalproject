219b22affd6d49a404c69b1e1ba1f2cb0bd4422b
BrandonChen0505 <121589746+BrandonChen0505@users.noreply.github.com>
2023-05-03 19:05:31 -0400

    Update rsi.m
    update

--- rsi.m
+++ rsi.m
@@ -7,12 +7,9 @@ function [S] = rsi(X, h, L, U)
     RS = (sum(UUt(end-h:end)) / h) / (sum(DDt(end-h:end)) / h);
     RSI = 100*(1 - 1/(1+RS));
 
-    RS_lag = (sum(UUt(end-h-1:end-1)) / h) / (sum(DDt(end-h-1:end-1)) / h);
-    RSI_lag = 100*(1 - 1/(1+RS_lag));
-
-    if RSI_lag < U && RSI > U
+    if RSI > U
         S = -1;
-    elseif RSI_lag > L && RSI < L
+    elseif RSI < L
         S = 1;
     else
         S = 0;
