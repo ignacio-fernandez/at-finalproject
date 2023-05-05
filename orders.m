b9b2bb025c766a94494ab2f481eacde353eb7757
Donald Donnelly <ddonnelly12@fordham.edu>
2023-05-05 14:37:09 -0400

    update orders and portfolio
    if orders or portfolio, return empty array

--- orders.m
+++ orders.m
@@ -1,9 +1,13 @@
 function [o] = orders()
     all_orders = IBMatlab('action','executions');
-    tickers = transpose({all_orders.symbol});
-    actn = transpose({all_orders.side});
-    quantity = transpose({all_orders.shares});
-    avgPx = transpose({all_orders.avgPrice});
-    time = transpose({all_orders.time});
-    [o] = [tickers, actn, quantity, avgPx, time];
+    if ~isempty(all_orders)
+        tickers = transpose({all_orders.symbol});
+        actn = transpose({all_orders.side});
+        quantity = transpose({all_orders.shares});
+        avgPx = transpose({all_orders.avgPrice});
+        time = transpose({all_orders.time});
+        [o] = [tickers, actn, quantity, avgPx, time];
+    else
+        [o] = [];
+    end
 end
