172fe26ef29fbdb1736147246c8b9b8d7d916204
Donald Donnelly <ddonnelly12@fordham.edu>
2023-05-05 10:34:10 -0400

    Update exe.m
    signal function calls weren't working

--- exe.m
+++ exe.m
@@ -4,9 +4,9 @@ for i = 1:length(bars)
     % calculate signals and combine to a 2d-vector, which is signals
     symbol = bar.symbol;
     close = bar.data.close;
-    macd_vec = macd(X=close, slow=26, macd_window=9, fast=12);
-    rsi_vec = rsi(X=close, h=6, L=20, U=80);
-    kdj_vec = kdj(bar_data=bar, n=9, m1=3);
+    macd_vec = macd(close, 26, 9, 12);
+    rsi_vec = rsi(close, 6, 20, 80);
+    kdj_vec = kdj(bar, 9, 3);
     signals = vertcat(macd_vec, rsi_vec, kdj_vec);
     orderId = execute_signals(dollar, symbol,signals, close);
     disp(orderId);
